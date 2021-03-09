eb986273a256ec77bdff1c23424f0b160af95a88
FrancescoPantaleoni <pantaleoni.francesco@gmail.com>
2021-03-09 18:58:03 +0100

    calendario funzionaa

---
+++ src/components/DateTimePicker.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+        <datetime format="YYYY/MM/DD" width="300px" @input="updatedCalendar" firstDayOfWeek="1" />
+        <!--<VueSlideBar
+        v-model="slider.value"
+        :data="slider.data"
+        :range="slider.range"
+        :labelStyles="{ color: '#003300', backgroundColor: '#003300' }"
+        :processStyle="{ backgroundColor: 'green' }"
+        @callbackRange="updatedSlider">
+        </VueSlideBar> -->
+    </div>
+</template>
+
+<script>
+import datetime from 'vuejs-datetimepicker';
+//import VueSlideBar from 'vue-slide-bar';
+export default {
+  name: 'DateTimePicker',
+  components: {
+      datetime,
+      //VueSlideBar
+  },
+  props: {
+      data: Array,
+  },
+  mounted() {
+  },
+  updated() {
+  },
+  methods: {
+    updatedCalendar(newDate) {
+        console.log("DatePicker::UpdatedCalendar val recieved::" + newDate);
+        this.$emit("pickedDate", newDate)
+    },
+   /* updatedSlider(newTime) {
+        console.log("Slider::updatedSlider new val: " + JSON.stringify(newTime) );
+        //this.$emit("pickedTime", this.);
+    }*/
+  }
+}
+</script>
---
+++ src/components/Map.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <p>ciao {{data}} </p>
+        <p v-for="d in data" :key="d"> {{d}}</p>
+
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Map',
+  props: {
+      data: Array,
+  },
+  components: {
+  },
+  mounted() {
+  },
+  updated() {
+      //console.log("UPDATED");
+  },
+  methods: {
+
+  }
+}
+</script>
